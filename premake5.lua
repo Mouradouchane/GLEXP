eca8b4fa6646c60808452d55afc405ecdac954d0
Mouradouchane <mouradouchane@gmail.com>
2025-02-08 00:03:39 +0100

    improves in premake build script

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,6 @@
-/
+
+include "help.lua"
+
 -- workspace/solution setup
 workspace("glexp_workspace") 
     configurations{ 
@@ -11,7 +13,7 @@ architecture("x64")
 -- project setup
 project("glexp_project")
 
-files {"**.cpp" , "**.hpp" , "**.h" , "**.c" , "**.vert" , "**.frag","**.ini"} 
+files {"**.cpp" , "**.hpp" , "**.h" , "**.c" , "**.vert" , "**.frag","**.ini" , "premake5.lua"} 
 
 -- program icon
 filter { 'system:windows' }
@@ -72,3 +74,59 @@ filter("configurations:release_dynamic")
     defines {"NDEBUG"}
     symbols("Off")
     optimize "Off"
+
+-- downloading libs
+print("============ LIBS SETUP:"..os.target().." ============")
+
+cwd = os.getcwd().."/"
+ok, err = os.mkdir("temp")
+copy_result = 0
+
+check_lib_files = check_files({ 
+    "temp/stb/stb_image.h" , 
+})
+
+if(check_lib_files==0)then
+    -- get stb_image
+    os.execute("git clone https://github.com/nothings/stb.git temp/stb")
+else 
+    print_hint("HINT::'stb' library found")
+end
+
+copy_result = _copy("temp/stb/stb_image.h" , "libs/")
+if copy_result == 0 then 
+    print("WARNING:: failed to copy stb_image to libs !")
+    print_warn("WARNING:: project not gonna compile correctly !")
+end
+
+check_lib_files = check_files({ 
+    "temp/mini/src/mini/ini.h" , 
+})
+
+if(check_lib_files==0)then
+    -- get mINI
+    os.execute("git clone https://github.com/metayeti/mINI.git/ temp/mini")
+else 
+    print_hint("HINT::'mINI' library found")
+end
+
+copy_result = _copy("temp/mini/src/mini/ini.h" , "libs/")
+if copy_result == 0 then 
+    print_warn("WARNING:: failed to copy mINI to libs !")
+    print_warn("WARNING:: project not gonna compile correctly !")
+end
+
+newoption {
+   trigger = "clean",
+   value = 1,
+   description = "removed downloaded content after build"
+}
+
+if(clean==1) then
+    clean_up = remove_temp_folder()
+    if(clean_up[0] == nil) then
+        print_warn("WARNING:: failed to delete temp folder")
+        print_error("ERROR:: " .. clean_up[1])
+        print_hint("HINT:: delete temp folder manualy !")
+    end
+end
