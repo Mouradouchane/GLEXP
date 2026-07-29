918b16314338499bc15e9fc2ce41859a3546faba
Mouradouchane <mouradouchane@gmail.com>
2026-07-29 07:38:12 +0100

    few improves on timer

--- core/time/timer.cpp
+++ core/time/timer.cpp
@@ -4,7 +4,18 @@
 #ifndef CORE_TIMER_CPP
 #define CORE_TIMER_CPP
 
+#include "timer.hpp"
 
+
+timer::timer() NOEXP {
+
+	this->start_point = GET_STEADY_TIME();
+
+}
+
+timer::~timer() NOEXP {
+
+}
 
 #endif
 #endif 
--- core/time/timer.hpp
+++ core/time/timer.hpp
@@ -12,24 +12,70 @@
 /*
 	few macros for timers
 */ 
-#define TIMER(FUNCTION) \
-		{\
-			timer t; \
-			FUNCTION;\
-		}
+#ifdef DEBUG
+	// todo: implement this
+	#define REGISTER_TIMER(VAR,TAG)
 
-#define TIMER_ASSIGN(VAR , FUNCTION) \
-		{\
-			timer t; \
-			VAR = FUNCTION;\
-		}
+	#define TIMER(FUNCTION) \
+			{\
+				timer t; \
+				FUNCTION;\
+				t.save();\
+			}
+#else 
+	#define REGISTER_TIMER(VAR,TAG)
+	#define TIMER(FUNCTION)
+#endif
 
+#define GET_STEADY_TIME()   std::chrono::steady_clock::now()
+#define GET_HIGH_RES_TIME() std::chrono::high_resolution_clock::now()
 
-// few types for time
+/*
+	few types for time
+*/ 
 typedef std::chrono::time_point<std::chrono::steady_clock> time_point;
-typedef std::chrono::duration<f64> time_elapse;
+typedef std::chrono::duration<u64> time_elapse;
 
-// to tell the timer used for what !
+/*
+	to tell the timer used for what !
+*/ 
+enum class timer_tag : u8;
+
+/*
+	timer class used for benchmark
+*/
+DLL_API_CLASS timer {
+
+private:
+	time_point  start_point;
+	time_elapse elapse;
+
+	u64 index = (u64)-1;
+
+#ifdef DEBUG
+	timer_tag tag = timer_tag::unkown;
+#endif
+
+public:
+	// constructor's
+	timer() NOEXP;
+	timer() NOEXP;
+
+	// destructor
+	~timer() NOEXP;
+
+	// public function's
+	void restart() NOEXP;
+	void save() NOEXP;
+
+	// get elpase time function's
+	u64 nanoseconds() NOEXP;
+	f32 miliseconds() NOEXP;
+	f32 seconds()     NOEXP;
+
+}; // class timer end
+
+
 enum class timer_tag : u8 {
 	unkown = 0,
 
@@ -52,34 +98,9 @@ enum class timer_tag : u8 {
 // enum timer_tag end
 
 
-DLL_API_CLASS timer {
-
-private:
-	time_point  start_point;
-	time_elapse elapse;
-
-	u64 index = (u64)-1;
-#ifdef DEBUG
-	timer_tag tag;
-#endif
-
-public:
-	 timer() NOEXP = default;
-	~timer() NOEXP;
-
-	void restart() NOEXP;
-
-	u64 nanoseconds() NOEXP;
-	u64 miliseconds() NOEXP;
-	f64 seconds()     NOEXP;
-
-}; // class timer end
-
-
 namespace core {
 
 	string time_to_string(timer t) NOEXP;
-
 }
 
 #endif 
